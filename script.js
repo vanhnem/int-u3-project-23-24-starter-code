220394350b60e0c7223413c68f53aa617dffab0c
vanhnem <vdarkhuah270@gmail.com>
2026-06-14 20:07:43 -0400

    all events are done

--- script.js
+++ script.js
@@ -21,12 +21,58 @@ optionOneBtn.addEventListener('click', function(){
     opening.style.display = 'none';
  });
 */
-let next1Btn = document.querySelection('.next1');
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+let next1Btn = document.querySelector('.next1');
+let scene2 = document.querySelector('.story-opening-2');
+let opening = document.querySelector('.story-opening');
+opening.style.display = 'block';
+next1Btn.addEventListener('click', function(){
+    scene2.style.display = 'block';
+    opening.style.display = 'none';
+ });
 
-// });
+let next2Btn = document.querySelector('.next2');
+let scene3 = document.querySelector('.story-opening-3');
+next2Btn.addEventListener('click', function(){
+    scene3.style.display = 'block';
+    scene2.style.display = 'none';
+ });
+
+ let next3Btn = document.querySelector('.next3');
+ let optionPage = document.querySelector('.story-opening-4');
+ next3Btn.addEventListener('click', function() {
+    optionPage.style.display = 'block';
+    scene3.style.display = 'none';
+ })
+
+ /* left turn code */
+ let leftBtn = document.querySelector('.option-one');
+ let leftScreen1 = document.querySelector('.option-one-screen');
+ leftBtn.addEventListener('click', function() {
+    leftScreen1.style.display = 'block';
+    optionPage.style.display = 'none';
+ })
+
+ let nextL = document.querySelector('.nextL');
+ let leftEnd = document.querySelector('.option-one-end');
+ nextL.addEventListener('click', function() {
+    leftEnd.style.display = 'block';
+    leftScreen1.style.display = 'none';
+ })
+
+ /* right turn code */
+ let rightBtn = document.querySelector('.option-two');
+ let rightScreen1 = document.querySelector('.option-two-screen');
+ rightBtn.addEventListener('click', function() {
+    rightScreen1.style.display = 'block';
+    optionPage.style.display = 'none';
+ })
+
+let nextR = document.querySelector('.nextR');
+let rightEnd = document.querySelector('.option-two-end');
+nextR.addEventListener('click', function() {
+    rightEnd.style.display = 'block';
+    rightScreen1.style.display = 'none';
+})
+ 
